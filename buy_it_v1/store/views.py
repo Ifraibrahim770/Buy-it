6e044038e9a708c3b4181842fb5167fe5d591d96
Ibrahim Diba <ibrahim.diba@turnkeyafrica.com>
2020-08-17 10:25:29 +0300

    category model

--- buy_it_v1/store/views.py
+++ buy_it_v1/store/views.py
@@ -7,8 +7,10 @@ from .models import *
 def store(request):
     products = Product.objects.all()
     carousel_images = CarouselImages.objects.all()
+    category_options = ProductCategory.objects.all()
     context = {'products': products,
-               'carousel_images': carousel_images}
+               'carousel_images': carousel_images,
+               'category_options': category_options}
     return render(request, 'store/store.html', context)
 
 
@@ -23,10 +25,18 @@ def checkout(request):
 
 
 def view(request, product_id):
+    # product_information
     product_info = Product.objects.filter(id=product_id)
     product_description = ProductDescription.objects.filter(product=product_id)
     product_images = ProductImage.objects.filter(product=product_id)
+
+    # similar_products
+    product_category = Product.objects.values_list('category', flat=True).get(pk=product_id)
+    similar_products = Product.objects.filter(category=product_category)
+
+    #context99
     context = {'product_info': product_info,
                'product_description': product_description,
-               'product_images': product_images}
+               'product_images': product_images,
+               'similar_products': similar_products}
     return render(request, 'store/view_product.html', context)
